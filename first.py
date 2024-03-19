e2b63a9f4da10eabe93b677d67b1e667bc0b689e
Ramunas P <ramunas.platukis@gmail.com>
2024-03-19 09:24:28 +0200

    Meniu ir pasirinkimų pakeitimai
    pašalintas 2meniu pasirinkimas, paredaguotas pasirinkimų tikrinimas ir printas po tikrinimo

--- first.py
+++ first.py
@@ -1,11 +1,11 @@
 while True:
     print("\n1. Atspausdinti įvestį atbulai"
-          "\n2. Atspausdinti įvestį ir simbolių įvestyje kiekį"
           "\nq  - išeiti")
     pasirinkimas = input(">>> ")
 
 #####################################
 # pasirinkimo patikrinimas
-    if pasirinkimas not in ("q", "1", "2"):
+    if pasirinkimas not in ("q", "1"):
+        print("--------------------")
         print("Tokio pasirinkimo nėra")
         continue
